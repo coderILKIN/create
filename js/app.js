acd7ea03fe2657c55cae5858d261289dc184a391
coderILKIN <tu758403c@code.edu.az>
2022-05-07 21:21:00 +0400

    first

---
+++ js/app.js
@@ -0,0 +1,223 @@
+// let num = 14;
+// num = 18;
+
+// console.log(num)
+
+// const secondNumber = 50;
+
+// console.log(secondNumber)
+
+
+// let firstName = "ILKIN";
+
+// firstName = 'YUSUBOV';
+
+// console.log(firstName)
+
+
+// let ask = true;
+
+// console.log(typeof num);
+
+// console.log(typeof firstName);
+
+// console.log(typeof ask);
+
+
+
+
+// let number = prompt("enter something please");
+
+// // console.log(number);
+
+
+// console.log(Number(number));
+
+
+
+
+
+// let str = "codeacademy";
+
+// let str2 = "Academy"
+
+// console.log(str.charAt(0));
+
+// console.log(str[1])
+
+// console.log(str.charCodeAt(0))
+
+// console.log(str.concat(str2));
+
+// let final = str.concat(str2);
+
+// console.log(final);
+
+// console.log(str.slice(0,3));
+// console.log(str2.substring(0,4));
+
+// console.log(str.replace("c","o"));
+// console.log(str.replaceAll("c","o"));
+
+// let username = "   ilkin yusubov   ";
+
+// console.log(username);
+
+// console.log(username.trim());
+
+// let str = "ILKIN";
+
+// console.log(str.length)
+
+// let firstName = "ILKIN";
+
+// console.log(firstName.toUpperCase());
+
+// console.log(firstName.toLowerCase());
+
+
+// console.log(firstName.indexOf("I"));
+
+// console.log(firstName.includes('I'));
+
+// let num = 4;
+
+// let num2 = "4";
+
+// console.log(num == num2);
+
+// console.log(num === num2);
+
+
+// let x = 10;
+
+// let y = 20;
+
+// console.log(x>y);
+
+// console.log(x<y);
+
+// console.log(x != y);
+
+// console.log(x <= y);
+
+
+// let x = true;
+
+// if(x == true) { 
+//     console.log("dogrudur")
+// }
+// else if(x == false) {
+//     console.log("yanslidir")
+// }
+// else{
+//     console.log("duzgun deyer ver")
+// }
+
+
+
+// let num1 = 21;
+
+// let num2 = 23;
+
+// let num3 = 33;
+
+// if(num1 > num2 || num2 < num3){
+//     console.log(num1,num2);
+// }
+// else{
+//     console.log("duzgun deyil");
+// }
+
+// if(num1<num2 && num2<num3){
+//     console.log(num1,num2);
+// }
+// else{
+//     console.log("duzgun deyil")
+// }
+
+
+// function firstName() {
+//     console.log("sagol");
+// }
+
+// firstName();
+
+// const secondName = () => {
+//     console.log("salam");
+// }
+
+// secondName();
+
+
+// function Firstfunc() {
+//     console.log("necesen");
+
+// }
+
+// Firstfunc();
+
+
+// const secontFunc = () => {
+//     console.log("salam arrow func");
+// }
+
+// secontFunc();
+
+
+
+// const Name = (num1,num2) => {
+//     console.log(num1+num2);
+// }
+
+// Name(20,15);
+
+
+// const Name = (name) => {
+//     if(name == "ILKIN"){
+//        console.log(name.slice(0,4));
+//     }
+//     else{
+//         console.log("adi duzgun daxil edin")
+//     }
+// }
+
+// Name("ILKIN");
+
+// let number = prompt("Enter name please");
+
+// const firsName = (num) => {
+//      if(num>0) {
+//          return num;
+//      }
+//      else{
+//          return "ededi duzgun daxil edin";
+//      }
+// }
+
+// console.log(firsName(number));
+
+
+
+
+// let arr = ['ilkin', 21, 13, {Name:"phone"}, null,undefined]
+
+// for(let name of arr){
+//     console.log(name);
+// }
+
+// for(let name in arr) {
+//     console.log(name);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
